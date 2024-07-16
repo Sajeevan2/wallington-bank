1999d3918dcc3cae95bf241b09632c4cd6313118
Sajeevan2 <mailtosajeeve@gmail.com>
2024-07-16 20:02:49 +0100

    implemented card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = {userName:'Sajeevan'}
+  const loggedIn = {firstName:'Ian',lastName:'Cooper',email:'ianCooper@hotmail.co.uk'}
   return (
     <section className='home'>
       <div className='home-content'>
@@ -20,7 +21,13 @@ const Home = () => {
               totalCurrentBalance={1250.35}
             />
         </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance:2320.87},{currentBalance:188.90}]}
+      />
     </section>
   )
 }
